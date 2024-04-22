9a3dae15015fdcb0c879a16086e1b30e40e07e8e
IYStoychev21 <IYStoychev21@codingburgas.bg>
2024-04-22 16:46:11 +0300

    Add server subfolder

--- server/start.py
+++ server/start.py
@@ -1,4 +1,4 @@
 import subprocess
 
 subprocess.run("apt install -y libpq5", shell=True)
-subprocess.run("./bin/Debug-linux-x86_64/Server/Server", shell=True)
+subprocess.run("./server/bin/Debug-linux-x86_64/Server/Server", shell=True)
